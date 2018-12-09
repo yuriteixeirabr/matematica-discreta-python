2a34c36dbcd943972f7f65fa39beaef453859040
Yuri Cordeiro Teixeira <yuri.c.teixeira@hotmail.com>
2018-12-09 17:52:35 -0300

    Questões 3 e 4

--- trabalho.py
+++ trabalho.py
@@ -5,7 +5,10 @@ Respostas = {
     "Formacao": {"Fundamental_Completo": 0, "Medio_Completo": 0, "Superior_Incompleto": 0, "Superior_Completo": 0,
                  "Graduacao": 0},
     "Problema": {"Sumario": 0, "Figura": 0, "Pagina": 0, "Espacamento": 0, "Alinhamento": 0, "Referencia": 0,
-                 "Citacao": 0, "Tabela": 0, "Compreensao": 0, "Outro": 0}}
+                 "Citacao": 0, "Tabela": 0, "Compreensao": 0, "Outro": 0},
+    "Ambiente": {"Google": 0, "Word": 0, "Libre": 0, "FastFormat": 0, "Quip": 0, "Zoho": 0, "Only": 0, "Outro": 0},
+    "Ferramenta": {"Modelo": 0, "Referencia": 0, "Tutorial": 0, "Sugestoes": 0, "Formatacao": 0, "Citacoes": 0,
+                   "Compartilhar": 0, "Grafico": 0, "Sumario": 0, "Plagio": 0, "Siglas": 0, "Imagens": 0, "Outro": 0}}
 with open('respostas.csv', 'r', encoding='utf-8', errors='ignore') as csvfile:
     spamreader = csv.DictReader(csvfile)
     for row in spamreader:
@@ -43,6 +46,52 @@ with open('respostas.csv', 'r', encoding='utf-8', errors='ignore') as csvfile:
                         Respostas['Problema']['Compreensao'] += 1
                     elif tipo_problema.lower() == 'outros':
                         Respostas['Problema']['Outro'] += 1
+            elif key.lower() == 'quais dos seguintes ambientes você utiliza na edição de seus trabalhos?':
+                for tipo_ambiente in val.split(';'):
+                    if tipo_ambiente.lower() == 'google docs':
+                        Respostas['Ambiente']['Google'] += 1
+                    elif tipo_ambiente.lower() == 'word':
+                        Respostas['Ambiente']['Word'] += 1
+                    elif tipo_ambiente.lower() == 'libre office':
+                        Respostas['Ambiente']['Libre'] += 1
+                    elif tipo_ambiente.lower() == 'fastformat':
+                        Respostas['Ambiente']['FastFormat'] += 1
+                    elif tipo_ambiente.lower() == 'quip':
+                        Respostas['Ambiente']['Quip'] += 1
+                    elif tipo_ambiente.lower() == 'zoho':
+                        Respostas['Ambiente']['Zoho'] += 1
+                    elif tipo_ambiente.lower() == 'only office':
+                        Respostas['Ambiente']['Only'] += 1
+                    elif tipo_ambiente.lower() == 'outros':
+                        Respostas['Ambiente']['Outro'] += 1
+            elif key.lower() == 'quais destas opções você gostaria que uma ferramenta de edição fornecesse como ajuda na edição de um trabalho acadêmico?':
+                for ferramenta_edicao in val.split(';'):
+                    if ferramenta_edicao.lower() == 'modelo de documentos':
+                        Respostas['Ferramenta']['Modelo'] += 1
+                    elif ferramenta_edicao.lower() == 'gerador de referências':
+                        Respostas['Ferramenta']['Referencia'] += 1
+                    elif ferramenta_edicao.lower() == 'tutorial interativo da ferramenta':
+                        Respostas['Ferramenta']['Tutorial'] += 1
+                    elif ferramenta_edicao.lower() == 'sugestões de erros':
+                        Respostas['Ferramenta']['Sugestoes'] += 1
+                    elif ferramenta_edicao.lower() == 'dicas de formatação':
+                        Respostas['Ferramenta']['Formatacao'] += 1
+                    elif ferramenta_edicao.lower() == 'instruções para citações':
+                        Respostas['Ferramenta']['Citacoes'] += 1
+                    elif ferramenta_edicao.lower() == 'compartilhar documento com o orientador':
+                        Respostas['Ferramenta']['Compartilhar'] += 1
+                    elif ferramenta_edicao.lower() == 'gerador de gráficos':
+                        Respostas['Ferramenta']['Grafico'] += 1
+                    elif ferramenta_edicao.lower() == 'criação automática de sumários':
+                        Respostas['Ferramenta']['Sumario'] += 1
+                    elif ferramenta_edicao.lower() == 'verificação de plágio':
+                        Respostas['Ferramenta']['Plagio'] += 1
+                    elif ferramenta_edicao.lower() == 'gerador de tabela de siglas':
+                        Respostas['Ferramenta']['Siglas'] += 1
+                    elif ferramenta_edicao.lower() == 'gerador de tabela de imagens':
+                        Respostas['Ferramenta']['Imagens'] += 1
+                    elif ferramenta_edicao.lower() == 'outros':
+                        Respostas['Ferramenta']['Outro'] += 1
 
 
 def tal_que(predicado, espaco):
@@ -93,7 +142,7 @@ def sexo_m(r):
     return 'Sexo_M' in r
 
 
-def superior_incompleto(r):
+def formacao_superior_incompleto(r):
     return 'Superior_Incompleto' in r
 
 
@@ -101,9 +150,39 @@ def problema_edicao(r):
     return 'Referencia' in r or 'Citacao' in r or 'Pagina' in r
 
 
+def problema_sumario(r):
+    return 'Sumario' in r
+
+
+def ambiente_word(r):
+    return 'Word' in r
+
+
+def ambiente_word_problema_sumario(r):
+    return 'Word' in r and 'Sumario' in r
+
+
+def ferramenta_criacao_sumario(r):
+    return 'Sumario' in r
+
+
 PDFormacao = ProbDist(Respostas['Formacao'])
 PDProblema = ProbDist(Respostas['Problema'])
 PDFormacaoProblema = joint(PDFormacao, PDProblema, ' ')
-PEX2 = P(superior_incompleto, tal_que(problema_edicao, PDFormacaoProblema))
+PEX2 = P(formacao_superior_incompleto, tal_que(problema_edicao, PDFormacaoProblema))
 """Imprimindo resposta da questão 2."""
-print(PEX2)
+# print(PEX2)
+
+
+PDAmbiente = ProbDist(Respostas['Ambiente'])
+PDProblemaAmbiente = joint(PDAmbiente, PDProblema, ' ')
+PEX3 = P(ambiente_word, tal_que(problema_sumario, PDProblemaAmbiente))
+"""Imprimindo resposta da questão 3."""
+# print(PEX3)
+
+
+PDFerramenta = ProbDist(Respostas['Ferramenta'])
+PDProblemaAmbienteFerramenta = joint(PDProblemaAmbiente, PDFerramenta, ' ')
+PEX4 = P(ambiente_word_problema_sumario, tal_que(ferramenta_criacao_sumario, PDProblemaAmbienteFerramenta))
+"""Imprimindo resposta da questão 4."""
+print(PEX4)
